0c5a192eaa61a7dac7ef8ae6a7999c06ed1f8ad1
wahab <aderintoadetunji@gmail.com>
2023-06-05 19:09:20 +0100

    added genres loading spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import buildCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {genres.map((genre) => (
