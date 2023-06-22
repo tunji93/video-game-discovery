454a5317419af61c1a5c839f60040e73cedcf410
wahab <aderintoadetunji@gmail.com>
2023-06-22 07:45:57 +0100

    refactored game-heading component

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,16 +1,22 @@
 import { Heading } from "@chakra-ui/react";
 import { GamesQuery } from "../App";
+import useGenres from "../hooks/useGenres";
+import usePlatforms from "../hooks/usePlatforms";
 
 type Props = {
   gamesQuery: GamesQuery;
 };
 function GameHeading({ gamesQuery }: Props) {
-  //   const heading = `${gamesQuery?.platform?.name || ""} ${
-  //     gamesQuery?.genre?.name || ""
-  //   } Games`;
+  const { data: platforms } = usePlatforms();
+  const platform = platforms?.results.find(
+    (p) => p.id === gamesQuery.platformId
+  );
+  const { data: genres } = useGenres();
+  const genre = genres?.results.find((g) => g.id === gamesQuery.genreId);
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
     <Heading as="h1" paddingY={5} fontSize="5xl">
-      {/* {heading} Wahab */} Wahab
+      {heading}
     </Heading>
   );
 }
